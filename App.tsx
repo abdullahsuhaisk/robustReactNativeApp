706f9f9a7a621189087af0eea6bd06a9bc09c97e
Süha <suha.isik@sisal.com>
2023-07-04 12:46:37 +0300

    ignore logbox

--- App.tsx
+++ App.tsx
@@ -5,16 +5,21 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 
 import configureStore from './src/core/configurestore';
 // import BottomTabNavigator from './src/navigations/BottomTabNavigator';
 import AppStack from './src/navigations/AppNavigator';
+import { LogBox } from 'react-native';
 
 function App(): JSX.Element {
   // const store = configureStore;
+  useEffect(() => {
+    LogBox.ignoreAllLogs();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Provider store={configureStore}>
